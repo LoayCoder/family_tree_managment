da8b2befdaea5b7f2da141fc7c13ca80f0d8119d
LOAY <40740097+LoayCoder@users.noreply.github.com>
2025-06-13 06:15:48 +0300

    Updated LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -68,7 +68,7 @@ export default function LandingPage({ onShowAuth }: LandingPageProps) {
           <div className="text-center">
             <div className="max-w-5xl mx-auto">
               <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-gray-800 mb-6 sm:mb-8 leading-tight text-center mb-100">
-                الأشراف بني عمير 
+                الأشرافبب بني عمير 
                 <span className="block bg-gradient-to-r from-emerald-600 to-blue-600 bg-clip-text text-transparent">
                   شرف النسب وعراقة التاريخ
                 </span>
